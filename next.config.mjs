9df7b7a47dd308a7eb58f71eb15dbc71e5b8fd75
Lai Weng Hong <117085597+shane062@users.noreply.github.com>
2024-07-12 18:48:23 +0800

    #commit:production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/portfolio-react",
+  trailingSlash: true,
+};
 
 export default nextConfig;
